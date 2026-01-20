7ef179a2aefc5e9928994863eae9e984dceb682a
Alexander <onixjkk@gmail.com>
2026-01-20 18:56:33 +0300

    done

---
+++ 8kyu/sum-mixed-array.js
@@ -0,0 +1,19 @@
+// ❓ DESCRIPTION:
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+// Return your answer as a number.
+
+// 👌 TESTS:
+// sumMix([9, 3, '7', '3']), 22
+// sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]), 42
+// sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']), 41
+
+// ✅ SOLUTION:
+
+function sumMix(x){
+    let sum = 0
+    for(let i = 0; i < x.length; i++) {
+        sum += +x[i]
+    }
+
+    return sum
+}
